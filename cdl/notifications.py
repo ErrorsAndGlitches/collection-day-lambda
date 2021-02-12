6bbdfb37340f7dc20755c628e5293c10f0027f44
Benjamyn Ward <benjamyn@amazon.com>
2021-02-11 19:24:29 -0800

    Add more logging and send message as TRANSACTIONAL type
    ## Problem
    I stopped getting SMS on my phone.
    
    ## Solution
    Using TRANSACTIONAL type fixed it. Also added more logging to help root cause issues for next time.
    
    ## Test
    * unit tests
    * updated lambda function

--- cdl/notifications.py
+++ cdl/notifications.py
@@ -16,8 +16,17 @@ class SnsNotification(object):
         self._phone_numbers = phone_numbers
 
     def send(self, msg):
+        print("Sending message: {}".format(msg))
         for phone_number in self._phone_numbers:
-            self._sns.publish(
+            print("Sending message to {}".format(phone_number))
+            response = self._sns.publish(
                 PhoneNumber=phone_number,
-                Message=msg
+                Message=msg,
+                MessageAttributes={
+                    'AWS.SNS.SMS.SMSType': {
+                        'DataType': 'String',
+                        'StringValue': 'Transactional'
+                    }
+                }
             )
+            print('Message ID: {}'.format(response['MessageId']))
--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -5,6 +5,13 @@ from cdl.notifications import SnsNotification, StdoutNotification
 
 
 class TestCollectionInfo(TestCase):
+    _MSG_ATTRIBUTES = {
+        'AWS.SNS.SMS.SMSType': {
+            'DataType': 'String',
+            'StringValue': 'Transactional'
+        }
+    }
+
     def test_sns_notification(self):
         sns = Mock()
         phone_numbers = [
@@ -12,12 +19,14 @@ class TestCollectionInfo(TestCase):
             '+11111111111',
         ]
         msg = 'message in a bottle'
+        sns.publish = Mock()
+        sns.publish.return_value = {'MessageId': '0123'}
         SnsNotification(sns, phone_numbers).send(msg)
 
         sns.publish.assert_has_calls(
             calls=[
-                call(PhoneNumber='+00000000000', Message=msg),
-                call(PhoneNumber='+11111111111', Message=msg)
+                call(PhoneNumber='+00000000000', Message=msg, MessageAttributes=self._MSG_ATTRIBUTES),
+                call(PhoneNumber='+11111111111', Message=msg, MessageAttributes=self._MSG_ATTRIBUTES)
             ],
             any_order=True
         )
